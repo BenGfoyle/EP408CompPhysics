069a39fc9c0c571931b1085e8069df507c1c1697
Benjamin Guilfoyle <bguilfoyle@physstaff.nuim.ie>
2019-12-06 12:58:26 +0000

    done

--- Week8/Ex3RNG.py
+++ Week8/Ex3RNG.py
@@ -8,38 +8,43 @@ from mpl_toolkits.mplot3d import Axes3D
 import random
 
 #===============================================================================
-def makePlot(x,y,z,name):
+def makePlot(x,y,z,name,col):
     """
     Overview: Make a plot of x vs y
     """
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(x, y, z)
-    fig.show()
+    ax.scatter(x, y, z, c = col, marker = ".")
 #===============================================================================
 
 #===============================================================================
-def RNG(i):
-    a,c,m,seed = 65, 319, 65537, 3434
-    if i == 0:
-        return seed
-    else:
-        return ((a * RNG(i - 1) + c) % m)
+def rng(n,x0):
+    a,c,m = 65, 319, 65537
+    prs = []
+    xn = x0
+    for i in range (n):
+        xn = ((a*xn) + c) % m
+        prs.append (xn)
+    return prs
+#===============================================================================
+
+#===============================================================================
+def rng2():
+    return random.uniform(0,1)
 #===============================================================================
 
 #===============================================================================
 def exerciseThree():
-    x = []
-    y = []
-    z = []
-    for i in range(1,100):
-        x.append(RNG(i))
-        y.append(RNG(i))
-        z.append(RNG(i))
+    x1,y1,z1 = rng(5000,56),rng(5000,100),rng(5000,54)
+    x2,y2,z2 = [],[],[]
+    for i in range(0,5000):
+        x2.append(rng2())
+        y2.append(rng2())
+        z2.append(rng2())
 
-    print(x,y,z)
 
-    makePlot(x,y,z,"RNG Plot")
+    makePlot(x1,y1,z1,"RNG Plot","r")
+    makePlot(x2,y2,z2,"RNG Plot","b")
     plt.show()
 #===============================================================================
 exerciseThree()
